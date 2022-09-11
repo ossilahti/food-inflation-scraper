83cf5889f787705f74190504cdd3c9e4318c36c5
ossilahti <ossi.lahti5@gmail.com>
2022-09-11 17:20:04 +0300

    Put everything inside class

--- index.js
+++ index.js
@@ -1,36 +1,54 @@
+import axios from 'axios';
+import cheerio from 'cheerio';
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+const app = express();
+app.use(cors());
+dotenv.config();
 
-const axios = require('axios')
-const cheerio = require('cheerio')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
-app.use(cors())
+export class MilkPrices {
+    constructor() {
+        this.url = 'https://www.s-kaupat.fi/tuote/valio-vapaan-lehman-kevytmaito-1-l/6408430000128';
+    }
 
-// Checks milk price from s-kaupat
-const url = 'https://www.s-kaupat.fi/tuote/valio-vapaan-lehman-kevytmaito-1-l/6408430000128';
+    start() {
+        app.get('/', function (req, res) {
+            res.json('This is a test');
+        });
+    };
 
-app.get('/', function (req, res) {
-    res.json('This is my webscraper')
-})
+    getPriceFromSKaupat() {
+        app.get('/results', (req, res) => {
+            axios(this.url)
+                .then(response => {
+                    const html = response.data;
+                    const $ = cheerio.load(html);
+                    const products = [];
+        
+                    $('.ant-row', html).each(function () { //<-- cannot be a function expression
+                        const title = $('.sc-618c7756-0').text();
+                        const price = $('.sc-d1a120d4-0').text();
+                        products.push({
+                            title,
+                            price
+                        });
+                    })
+                    res.json(products);
+                }).catch(err => console.log(err))
+        });
+    }
 
-app.get('/results', (req, res) => {
-    axios(url)
-        .then(response => {
-            const html = response.data
-            const $ = cheerio.load(html)
-            const products = []
+    activate() {
+        this.start();
+        this.getPriceFromSKaupat();
+    }
+}
 
-            $('.ant-row', html).each(function () { //<-- cannot be a function expression
-                const title = $('.sc-618c7756-0').text();
-                const price = $('.sc-d1a120d4-0').text();
-                products.push({
-                    title,
-                    price
-                });
-            })
-            res.json(products);
-        }).catch(err => console.log(err))
-})
+function activateViewModel() {
+    app.listen(process.env.PORT, () => console.log(`server running on PORT ${process.env.PORT}`));
+    const milkPrices = new MilkPrices();
+    milkPrices.activate();
+}
 
-app.listen(process.env.PORT, () => console.log(`server running on PORT ${process.env.PORT}`))
+activateViewModel();
