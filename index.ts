3f4cbcd9f4dcaebec9628b42c370ac4d5fa3fb4d
ossilahti <ossi.lahti5@gmail.com>
2022-09-11 23:16:53 +0300

    Refactor classes and add documentation

--- index.ts
+++ index.ts
@@ -15,42 +15,50 @@ export class Prices {
         this.cheeseUrl = 'https://www.s-kaupat.fi/tuote/valio-oltermannir-17-e900-g/6408430039517';
     }
 
-    async getDetails(url: any) {
+    /**
+     * Gets the html data and makes it readable
+     * @param url The url for what product the data is fetched
+     * @returns Html data of given url
+     */
+    async getDetails(url: string) {
         const response = await axios.get(url);
         return response.data;
+    }
+
+    /**
+     * Iterates through html data by class names
+     * @param data The html data
+     * @param parentClass The parent element in html - under this is found the price details
+     * @param titleClass Under this class is the text of the product title
+     * @param priceClass Under this class is the text of the product price
+     * @returns Object array of title and price - returns it in json to server
+     */
+    fetchPriceData(data: any, parentClass: string, titleClass: string, priceClass: string) {
+        let priceDetails: { title: string, price: string} [] = [];
+        const $ = cheerio.load(data);
+        $(parentClass, data).each((i) => { 
+            priceDetails[0] = {
+                title: $(titleClass).text(),
+                price: $(priceClass).text()
+            };
+        });
+
+        return priceDetails;
     }
 
     getMilkPrice() {
         app.get('/milk', async (req: any, res: any) => {
             const response = await this.getDetails(this.milkUrl);
-            const $ = cheerio.load(response);
-            let products: { title: string, price: string} [] = [];
-            
-            $('.ant-row', response).each((i) => { 
-                products[0] = {
-                    title: $('.sc-618c7756-0').text(),
-                    price: $('.sc-d1a120d4-0').text()
-                };
-            });
-
-            res.json(products);
+            const milkPrice = this.fetchPriceData(response, '.ant-row', '.sc-618c7756-0', '.sc-d1a120d4-0');
+            res.json(milkPrice);
         });
     }
 
     getCheesePrice() {
         app.get('/cheese', async (req: any, res: any) => {
             const response = await this.getDetails(this.cheeseUrl);
-            const $ = cheerio.load(response);
-            let cheeses: { title: string, price: string} [] = [];
-
-            $('.sc-76652cbd-2', response).each(() => {
-                cheeses[0] = {
-                    title: $('.sc-618c7756-0').text(),
-                    price: $('.sc-d1a120d4-0').text()
-                };
-            });
-            
-            res.json(cheeses);
+            const cheesePrice = this.fetchPriceData(response, '.sc-76652cbd-2', '.sc-618c7756-0', '.sc-d1a120d4-0');
+            res.json(cheesePrice);
         });
     }
 
@@ -62,8 +70,8 @@ export class Prices {
 
 function activateViewModel() {
     app.listen(process.env.PORT, () => console.log(`server running on PORT ${process.env.PORT}`));
-    const sRyhma = new Prices();
-    sRyhma.activate();
+    const priceClass = new Prices();
+    priceClass.activate();
 }
 
 activateViewModel();
